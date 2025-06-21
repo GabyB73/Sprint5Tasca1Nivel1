e3870da18451246a2ec6bb320e4cc7fb272220b7
GabyB73 <geb.beg.73@gmail.com>
2025-06-21 16:08:33 +0200

    Actualizar controlador SucursalController para cumplir con los endpoints del enunciado

--- src/main/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s05/t01/n01/S05T01N01BustamanteGabriela/controller/SucursalController.java
+++ src/main/java/cat/itacademy/barcelonactiva/bustamante/gabriela/s05/t01/n01/S05T01N01BustamanteGabriela/controller/SucursalController.java
@@ -13,48 +13,53 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/sucursales") // Ruta base para todos los endpoints del controlador
+@RequestMapping("/sucursal") // Ruta base para todos los endpoints del controlador
 public class SucursalController {
     @Autowired
     private SucursalServicio sucursalServicio;
-@PostMapping("/add")
-public ResponseEntity<SucursalDTO> createSucursal(@RequestBody Sucursal sucursal) {
-    Sucursal nuevaSucursal = sucursalServicio.crearSucursal(sucursal);
-    SucursalDTO nuevaSucursalDTO = sucursalServicio.convertToDto(nuevaSucursal);
-    return ResponseEntity.status(HttpStatus.CREATED).body(nuevaSucursalDTO);
 
-}
-@PutMapping("/update/{id}")
-    public ResponseEntity<String> updateSucursal(@PathVariable int id, @RequestBody Sucursal sucursalActualizada) {
-    try {
-        sucursalServicio.updateSucursal(id, sucursalActualizada.getNombreSucursal(), sucursalActualizada.getPaisSucursal());
-        return ResponseEntity.ok("Sucursal actualizada exitosamente");
-    } catch (SucursalNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @PostMapping("/add")
+    public ResponseEntity<SucursalDTO> createSucursal(@RequestBody Sucursal sucursal) {
+        Sucursal nuevaSucursal = sucursalServicio.crearSucursal(sucursal);
+        SucursalDTO nuevaSucursalDTO = sucursalServicio.convertToDto(nuevaSucursal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaSucursalDTO);
+
     }
 
-}
-@GetMapping("/getAll")
-public ResponseEntity<List<SucursalDTO>> getAllSucursal() {
-    List<SucursalDTO> sucursales = sucursalServicio.getAllSucursal();
-    return ResponseEntity.ok(sucursales);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateSucursal(@PathVariable int id, @RequestBody Sucursal sucursalActualizada) {
+        try {
+            sucursalServicio.updateSucursal(id, sucursalActualizada.getNombreSucursal(), sucursalActualizada.getPaisSucursal());
+            return ResponseEntity.ok("Sucursal actualizada exitosamente");
+        } catch (SucursalNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
 
-}
-@GetMapping("/{id}")
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<SucursalDTO>> getAllSucursal() {
+        List<SucursalDTO> sucursales = sucursalServicio.getAllSucursal();
+        return ResponseEntity.ok(sucursales);
+
+    }
+
+    @GetMapping("/getOne/{id}")
     public ResponseEntity<Sucursal> searchSucursalById(@PathVariable int id) {
-    Optional<Sucursal> sucursalOptional = sucursalServicio.getSucursalById(id);
+        Optional<Sucursal> sucursalOptional = sucursalServicio.getSucursalById(id);
 
-    return sucursalOptional
-            .map(sucursal -> ResponseEntity.ok().body(sucursal))  // si la sucursal existe
-            .orElseGet(() -> ResponseEntity.notFound().build());  //si la sucursal no existe
+        return sucursalOptional
+                .map(sucursal -> ResponseEntity.ok().body(sucursal))  // si la sucursal existe
+                .orElseGet(() -> ResponseEntity.notFound().build());  //si la sucursal no existe
 
-}
-@DeleteMapping("/delete/{id}")
+    }
+
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteSucursalById(@PathVariable int id) {
-    sucursalServicio.deleteSucursalById(id);
-    return ResponseEntity.noContent().build();
+        sucursalServicio.deleteSucursalById(id);
+        return ResponseEntity.noContent().build();
 
-}
+    }
 
 }
 
